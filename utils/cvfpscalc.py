d5da681bdd33f4053ae0f99ecb4dd7d91aa68167
naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2022-05-13 23:10:05 +0900

    cv2 as cv2

--- utils/cvfpscalc.py
+++ utils/cvfpscalc.py
@@ -1,15 +1,17 @@
+# forked from https://github.com/Kazuhito00/cvfpscalc
+
 from collections import deque
-import cv2 as cv
+import cv2
 
 
 class CvFpsCalc(object):
     def __init__(self, buffer_len=1):
-        self._start_tick = cv.getTickCount()
-        self._freq = 1000.0 / cv.getTickFrequency()
+        self._start_tick = cv2.getTickCount()
+        self._freq = 1000.0 / cv2.getTickFrequency()
         self._difftimes = deque(maxlen=buffer_len)
 
     def get(self):
-        current_tick = cv.getTickCount()
+        current_tick = cv2.getTickCount()
         different_time = (current_tick - self._start_tick) * self._freq
         self._start_tick = current_tick
 
